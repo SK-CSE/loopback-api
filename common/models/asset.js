01a5376ffb06ad765420d45f6428c29511256f4b
Saurabh Kumar <saurabh.cse04@gmail.com>
2019-09-09 00:20:14 +0530

    custom validator

--- common/models/asset.js
+++ common/models/asset.js
@@ -1,6 +1,23 @@
 'use strict';
 
 module.exports = function (Asset) {
+
+    // standard set of validators
+    Asset.validatesUniquenessOf('serialNumber', { 
+        message:'serialNumber is not unique'
+    });
+
+    // custom validator
+    Asset.validate('description', customValidator, {
+        message:'bad name'
+    })
+
+    function customValidator(err){
+        if(this.description === 'description'){
+            err();
+        }
+    }
+    
     // override built-in CRUD function
     Asset.on('dataSourceAttached', function(obj){
         Asset.deleteById = function(id, cb) {
